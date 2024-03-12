108da63aa79201a526d0c9ec447ed337d23f6086
muhammedrishadpe <rishadpe@gmail.com>
2024-03-12 12:02:06 +0100

    add PUT method

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 const cors = require("cors");
+const { todo } = require("node:test");
 
 app.use(cors());
 app.use(express.json());
@@ -37,11 +38,32 @@ app.post("/api/todo", (req, res) => {
   todoList.push(todoItem);
   res.json(todoList);
 });
+app.put("/api/todo", (req,res) => {
+    
+    const {id, todo, isCompleted} = req.body;
+const isExist = todoList.find(data=> data.id ===id);
+if(isExist) {
+    todoList.forEach((todoItem) => {
+        if(todoItem.id === id) {
+            todoItem.todo = todo;
+            todoItem.isCompleted = isCompleted || false;
+        }
+    });
+    return res.json(todoList);
+}
+
+res.status(404).json({
+    message: `Item with id:${id} does not exist`,
+});
+   
+});
+
+// app.delete("/api/todo")
+
 app.all("*", (req, res) => {
   res.status(404).json("This page does not exist");
 });
-// app.put("/api/todo")
-// app.delete("/api/todo")
+
 const PORT = 3005;
 
 app.listen(PORT, () => console.log(` server running on ${PORT}`));
