ebeadbc2001059455d6f35de2307c71776e5e177
muhammedrishadpe <rishadpe@gmail.com>
2024-03-12 07:29:06 +0100

    add uuid package to generate unique id and add post method

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require("express");
+const {v4:uuidv4} = require("uuid");
 const app =express();
+const cors = require("cors");
 
-const PORT = 3005;
+app.use(cors());
+app.use(express.json());
+
+const todoList = [{
+    id:"123",
+    todo:"test1",
+    isCompleted : false,
+}];
 
 app.get("/api/todo", (req,res) => {
-    res.json("Get Method");
+    res.status(200).json(todoList);
+    
+    
+});
+app.post("/api/todo", (req, res) => {
+    const {todo} = req.body;
+const todoItem = {
+    id:uuidv4(),
+    todo:todo,
+    isCompleted:false,
+};
+todoList.push(todoItem);
+res.json(todoList);
 });
 app.all("*", (req,res) => {
     res.status(404).json("This page does not exist");
 });
-
-
-// app.post("/api/todo")
 // app.put("/api/todo")
 // app.delete("/api/todo")
+const PORT = 3005;
 
 app.listen(PORT , () => console.log(` server running on ${PORT}`))
