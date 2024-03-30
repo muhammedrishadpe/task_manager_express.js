4bd1eac4e4483ed2696889214080decbb59ae371
muhammedrishadpe <rishadpe@gmail.com>
2024-03-30 14:46:37 +0100

    integrate to DB updated GET,PUT,DELETE,POST

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
-const todoSchema = mongoose.Schema({
-    todo:String,
-    isCompleted:Boolean
+const todoSchema = new mongoose.Schema({
+  todo: {
+    type:String,
+    minLength:5,
+    maxLength:10,
+    required:true
+  }  ,
+  isCompleted:{
+    type:String,
+    required:[true, "this field required by DB"], // just a demo
+  }
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -18,59 +18,71 @@ router.get("/", async (req, res) => {
     res.status(200).json(todoList);
   });
   router.post("/", async(req, res) => {
-    const { todo } = req.body;
-  
-    // if (!("todo" in req.body)) {
-    //   res.status(400).json({
-    //     message: `${JSON.stringify(
-    //       req.body
-    //     )}: This attribute is not accepted, Required attributes: todo`,
-    //   });
-    //   return;
-    // }
-  try {
-    
+         // if (!("todo" in req.body)) {
+          //   res.status(400).json({
+              //     message: `${JSON.stringify(
+                  //       req.body
+                  //     )}: This attribute is not accepted, Required attributes: todo`,
+                  //   });
+                  //   return;
+                  // }
+                  try {
+      const { todo } = req.body;
     const todoItem = {
         todo: todo,
         isCompleted: false,
       }; 
      await Todo.create(req.body);
-      res.json({message:"data"});
+      res.json({message:"data has been added"});
+  } catch (error) {
+  res.status(400).json({
+    message:error.message,
+  });
+  }
+});
+  router.put("/", async(req,res) => {
+  try {
+    const {_id, todo, isCompleted} = req.body;
+    const isExist = todoList.find(data=> data.id ===id);
+  const fieldsToUpdate = {
+      todo,
+      isCompleted
+  }
+    const updatedData = await Todo.findByIdAndUpdate(_id, fieldsToUpdate, {new:true})
+    if(updatedData) {
+        
+        return res.json(updatedData);
+    }
+    
+    res.status(404).json({
+        message: `Item with id:${id} does not exist`,
+    });
   } catch (error) {
     
-  }res.status(400).json({
-    message:error.message,
-  })
-  });
-  router.put("/", (req,res) => {
-      
-      const {id, todo, isCompleted} = req.body;
-  const isExist = todoList.find(data=> data.id ===id);
-  if(isExist) {
-      todoList.forEach((todoItem) => {
-          if(todoItem.id === id) {
-              todoItem.todo = todo;
-              todoItem.isCompleted = isCompleted || false;
-          }
-      });
-      return res.json(todoList);
+    res.status(404).json({
+        message:error.message,
+    });
   }
-  
-  res.status(404).json({
-      message: `Item with id:${id} does not exist`,
-  });
      
   });
   
-  router.delete("/", (req,res) => {
-  const {id} = req.body;
-  const todoIndex = todoList.findIndex((todoItem) => todoItem.id === id);
-  if(todoIndex !== -1) {
-      todoList.splice(todoIndex,1);
-      return res.json(todoList);
+  router.delete("/",async (req,res) => {
+try {
+    const {_id} = req.body;
+const deletedField = await Todo.findByIdAndDelete(_id)
+//   const todoIndex = todoList.findIndex((todoItem) => todoItem.id === id);
+  if(deletedField) {
+      return res.json({
+        message:"Record has been deleted",
+      });
   };
   
   res.status(404).json({message: "Item does not exist"})
-  });
+  
+} catch (error) {
+    res.status(404).json({message:error.message})
+    
+}  
+});
   
   module.exports = router;
